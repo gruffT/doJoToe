a3dd57257d6b5271c429fdf3f99ca43226ded0d5
Gareth Tomlinson <garetht@garethtomlinson.co.uk>
2022-05-26 13:05:46 +0100

    Full failing spec file

--- src/GameGrid/WinnerCheck.test.ts
+++ src/GameGrid/WinnerCheck.test.ts
@@ -1,17 +1,127 @@
-describe('WinnerCheck checkForWinner', () => {
-  it('should return undefined if no winner', () => {
-    expect('this').toBe('implemented');
+import { checkForWinner } from './WinnerCheck';
+import {
+  TIE, VALUE_X, WINNER_O, WINNER_X,
+} from '../Status/Statuses';
+import { VALUE_O } from './Square/Square';
+
+describe('WinnerCheck', () => {
+  it('should return WINNER_X for X in any row', () => {
+    expect(checkForWinner([
+      VALUE_X, VALUE_X, VALUE_X,
+      undefined, undefined, undefined,
+      undefined, undefined, undefined,
+    ])).toEqual(WINNER_X);
+    expect(checkForWinner([
+      undefined, undefined, undefined,
+      VALUE_X, VALUE_X, VALUE_X,
+      undefined, undefined, undefined,
+    ])).toEqual(WINNER_X);
+    expect(checkForWinner([
+      undefined, undefined, undefined,
+      undefined, undefined, undefined,
+      VALUE_X, VALUE_X, VALUE_X,
+    ])).toEqual(WINNER_X);
   });
 
-  it('should return WINNER_X for winning combinations of VALUE_X for a 3x3 board stored in 1D array packed row first', () => {
-    expect('this').toBe('implemented');
+  it('should return WINNER_X for X in any column', () => {
+    expect(checkForWinner([
+      VALUE_X, undefined, undefined,
+      VALUE_X, undefined, undefined,
+      VALUE_X, undefined, undefined,
+    ])).toEqual(WINNER_X);
+
+    expect(checkForWinner([
+      undefined, VALUE_X, undefined,
+      undefined, VALUE_X, undefined,
+      undefined, VALUE_X, undefined,
+    ])).toEqual(WINNER_X);
+
+    expect(checkForWinner([
+      undefined, undefined, VALUE_X,
+      undefined, undefined, VALUE_X,
+      undefined, undefined, VALUE_X,
+    ])).toEqual(WINNER_X);
   });
 
-  it('should return WINNER_O for winning combinations of VALUE_O for a 3x3 board stored in 1D array packed row first', () => {
-    expect('this').toBe('implemented');
+  it('should return WINNER_X for X in any diagonal', () => {
+    expect(checkForWinner([
+      VALUE_X, undefined, undefined,
+      undefined, VALUE_X, undefined,
+      undefined, undefined, VALUE_X,
+    ])).toEqual(WINNER_X);
+
+    expect(checkForWinner([
+      undefined, undefined, VALUE_X,
+      undefined, VALUE_X, undefined,
+      VALUE_X, undefined, undefined,
+    ])).toEqual(WINNER_X);
   });
 
-  it('should return TIE if no winner and all moves taken', () => {
-    expect('this').toBe('implemented');
+  it('should return WINNER_O for O in any row', () => {
+    expect(checkForWinner([
+      VALUE_O, VALUE_O, VALUE_O,
+      undefined, undefined, undefined,
+      undefined, undefined, undefined,
+    ])).toEqual(WINNER_O);
+    expect(checkForWinner([
+      undefined, undefined, undefined,
+      VALUE_O, VALUE_O, VALUE_O,
+      undefined, undefined, undefined,
+    ])).toEqual(WINNER_O);
+    expect(checkForWinner([
+      undefined, undefined, undefined,
+      undefined, undefined, undefined,
+      VALUE_O, VALUE_O, VALUE_O,
+    ])).toEqual(WINNER_O);
+  });
+
+  it('should return WINNER_O for O in any column', () => {
+    expect(checkForWinner([
+      VALUE_O, undefined, undefined,
+      VALUE_O, undefined, undefined,
+      VALUE_O, undefined, undefined,
+    ])).toEqual(WINNER_O);
+
+    expect(checkForWinner([
+      undefined, VALUE_O, undefined,
+      undefined, VALUE_O, undefined,
+      undefined, VALUE_O, undefined,
+    ])).toEqual(WINNER_O);
+
+    expect(checkForWinner([
+      undefined, undefined, VALUE_O,
+      undefined, undefined, VALUE_O,
+      undefined, undefined, VALUE_O,
+    ])).toEqual(WINNER_O);
+  });
+
+  it('should return WINNER_O for O in any diagonal', () => {
+    expect(checkForWinner([
+      VALUE_O, undefined, undefined,
+      undefined, VALUE_O, undefined,
+      undefined, undefined, VALUE_O,
+    ])).toEqual(WINNER_O);
+
+    expect(checkForWinner([
+      undefined, undefined, VALUE_O,
+      undefined, VALUE_O, undefined,
+      VALUE_O, undefined, undefined,
+    ])).toEqual(WINNER_O);
+  });
+
+  it('should return TIE for a tie', () => {
+    expect(checkForWinner([
+      VALUE_O, VALUE_X, VALUE_O,
+      VALUE_X, VALUE_X, VALUE_O,
+      VALUE_X, VALUE_O, VALUE_X,
+    ])).toEqual(TIE);
+  });
+
+  it('should return undefined for no result', () => {
+    expect(checkForWinner([
+      VALUE_O, VALUE_X, VALUE_O,
+      VALUE_X, undefined, VALUE_O,
+      VALUE_X, VALUE_O, VALUE_X,
+    ])).toEqual(undefined);
   });
 });
